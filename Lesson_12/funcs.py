aeeede3ee71ca2c2527a4d5e894f94afd310676f
XirEx-afk <mareevmaksim419@gmail.com>
2026-01-11 16:26:58 +0300

    Add Lesson 16

---
+++ Lesson_12/funcs.py
@@ -0,0 +1,5 @@
+def print_text(label, text):
+    label.config(text=f"Вы ввели: {text}")
+
+def color_change(label,fg):
+    label.config(fg="Blue")
--- Python/Lesson_15/hospital.py
+++
@@ -1,7 +0,0 @@
-class Doctor:
-    def __init__(self, name, patient_list):
-        self._name = name
-        self.patient_list = patient_list
-
-    def get_activity(self):
-        print(f"Доктор {self._name} проводит операцию")
